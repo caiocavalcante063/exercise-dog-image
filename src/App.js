549e349d8d39a19446c12a72f4e4b97949b6cb49
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-12 22:41:11 -0300

    resolve exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: undefined,
+    };
+
+    this.fetchRandomDogImage = this.fetchRandomDogImage.bind(this);
+  };
+
+  async fetchRandomDogImage() {
+    this.setState(
+      { image: undefined },
+      async () => {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const jsonResponse = await response.json();
+        const { message } = jsonResponse;
+        this.setState({
+          image: message,
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchRandomDogImage();
+  }
+
+  renderRandomDogImage() {
+    const { image } = this.state;
+    return (
+      <>
+        <img src={image} alt="dog aleatório do dia" />
+      </>
+    );
+  };
+
+  render() {
+    const loadingMessage = <span>loading...</span>
+    const { image } = this.state
+
+    return (
+      <div className="App">
+        <h1>Dog aleatório do dia:</h1>
+        {image ? this.renderRandomDogImage() : loadingMessage}
+        <button type="button" onClick={this.fetchRandomDogImage}>quer ver mais um?</button>
+      </div>
+    );
+  }
+};
 
 export default App;
